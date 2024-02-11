ef4f1c1292cf09c689a587a40ac1a37b40b2f96b
Atanu Paul <atanupaul2411@gmail.com>
2024-02-11 13:49:56 +0530

    google generative AI integrated properly

--- src/Components/InputBar.tsx
+++ src/Components/InputBar.tsx
@@ -1,16 +1,27 @@
 import {
   Keyboard,
+  NativeSyntheticEvent,
   StyleSheet,
-  Text,
   TextInput,
+  TextInputContentSizeChangeEventData,
+  TouchableWithoutFeedback,
   View,
   useColorScheme,
 } from 'react-native';
-import React from 'react';
+import React, {RefObject} from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome6';
 
-const InputBar = () => {
+interface InputBarProps {
+  setText: (value: string) => void;
+  runChat: () => void;
+}
+
+const InputBar: React.FC<InputBarProps> = ({setText, runChat}) => {
   const [textInput, setTextInput] = React.useState<string>('');
   const [marginBottom, setMarginBottom] = React.useState<number>(0);
+  const [keyboardEnabled, setKeyboardEnabled] = React.useState(true);
+  const [inputHeight, setInputHeight] = React.useState(40);
+  const colorMode = useColorScheme() === 'dark' ? '#fff' : '#000';
   const Themecolor = useColorScheme() === 'dark' && {
     backgroundColor: '#66666666',
   };
@@ -36,17 +47,73 @@ const InputBar = () => {
     };
   }, []);
 
+  const textInputRef: RefObject<TextInput> = React.useRef<TextInput>(null);
+
+  const toggleKeyboard = () => {
+    if (keyboardEnabled) {
+      textInputRef.current?.blur();
+    } else {
+      textInputRef.current?.focus();
+    }
+    setKeyboardEnabled(!keyboardEnabled);
+    setKeyboardEnabled(!keyboardEnabled);
+  };
+
+  const handleContentSizeChange = (
+    event: NativeSyntheticEvent<TextInputContentSizeChangeEventData>,
+  ) => {
+    setInputHeight(event.nativeEvent.contentSize.height);
+  };
+
+  const inputHandler = (value: string) => {
+    setTextInput(value);
+    setText(value);
+  };
+
   return (
     <View style={[styles.textInputContainer, {marginBottom}, Themecolor]}>
       <TextInput
+        ref={textInputRef}
         value={textInput}
-        onChangeText={setTextInput}
-        style={[useColorScheme() === 'dark' ? {color: '#fff'} : {color: '#000'}, {fontSize: 16}]}
+        onChangeText={text => inputHandler(text)}
+        style={{
+          color: colorMode,
+          height: Math.min(160, inputHeight),
+          fontSize: 16,
+          width: '85%',
+        }}
+        multiline={true}
+        numberOfLines={6}
+        textAlignVertical="top"
+        onContentSizeChange={handleContentSizeChange}
         autoCorrect={false}
-        autoCapitalize='sentences'
+        autoCapitalize="sentences"
         placeholder="Ask me anything..."
         placeholderTextColor={useColorScheme() === 'dark' ? '#bbb' : '#888'}
       />
+      {textInput.trim().length === 0 ? (
+        <TouchableWithoutFeedback onPress={toggleKeyboard}>
+          <Icon
+            name={'keyboard'}
+            size={20}
+            color={colorMode}
+            style={{paddingRight: 10}}
+          />
+        </TouchableWithoutFeedback>
+      ) : (
+        <TouchableWithoutFeedback
+          onPress={() => {
+            runChat();
+            setTextInput('');
+          }}>
+          <Icon
+            name={'paper-plane'}
+            size={20}
+            color={colorMode}
+            style={{paddingRight: 10}}
+          />
+        </TouchableWithoutFeedback>
+      )}
     </View>
   );
 };
@@ -55,6 +122,9 @@ export default InputBar;
 
 const styles = StyleSheet.create({
   textInputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
     backgroundColor: '#fff',
     borderRadius: 25,
     paddingHorizontal: 10,
--- src/Screens/ChatScreen.tsx
+++ src/Screens/ChatScreen.tsx
@@ -12,10 +12,75 @@ import React from 'react';
 import InputBar from '../Components/InputBar';
 import Icon from 'react-native-vector-icons/FontAwesome6';
 
+//generative AI impports
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+  InputContent,
+} from '@google/generative-ai';
+import {Google_API_KEY} from '../../API';
+
 const {height, width} = Dimensions.get('window');
 
 const ChatScreen: React.FC = () => {
+  const [textInput, setTextInput] = React.useState<string>('');
+  const [chatHistory, setChatHistory] = React.useState<InputContent[]>([]);
   const colorMode = useColorScheme() === 'dark' ? '#fff' : '#000';
+
+  const updateChatHistory = (role: string, content: string) => {
+    setChatHistory(prevHistory => [
+      ...prevHistory,
+      {
+        role: role,
+        parts: [{text: content}],
+      },
+    ]);
+  };
+
+  async function runChat() {
+    updateChatHistory('user', textInput);
+    const genAI = new GoogleGenerativeAI(Google_API_KEY);
+    const model = genAI.getGenerativeModel({model: 'gemini-pro'});
+
+    const generationConfig = {
+      temperature: 0.9,
+      topK: 1,
+      topP: 1,
+      maxOutputTokens: 2048,
+    };
+
+    const safetySettings = [
+      {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+    ];
+
+    const chat = model.startChat({
+      generationConfig,
+      safetySettings,
+      history: chatHistory,
+    });
+
+    const result = await chat.sendMessage(textInput!);
+    const response = result.response;
+    updateChatHistory('model', response.text());
+    console.log(response.text());
+  }
+
   return (
     <KeyboardAvoidingView behavior={'height'} style={styles.container}>
       <View style={styles.headContainer}>
@@ -27,9 +92,10 @@ const ChatScreen: React.FC = () => {
           <Icon name={'ellipsis-vertical'} color={colorMode} size={20} />
         </TouchableWithoutFeedback>
       </View>
+      <Text>Hello</Text>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View style={styles.inputBarContainer}>
-          <InputBar />
+          <InputBar setText={setTextInput} runChat={runChat} />
         </View>
       </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
